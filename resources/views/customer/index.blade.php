a1f01c590f6b54ea5a72e6de8fe5c4bfbd66e069
Mohamed kaffouh <87676772+Medkaffouh@users.noreply.github.com>
2022-04-28 10:35:50 +0000

    create the form control

---
+++ resources/views/customer/index.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Developer</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Customer</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">About</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+
+        <form class="mt-5">
+            <h1>Form Input Data</h1>
+            <div class="mb-3 mt-5">
+                <label class="form-label">Name</label>
+                <input type="text" class="form-control" name="name" value="{{ old('name') }}"
+                    placeholder="type your name" required>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Email</label>
+                <input type="email" class="form-control" name="email" value="{{ old('email') }}"
+                    placeholder="type your email" required>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Legende</label>
+                <input type="text" class="form-control" name="legende" value="{{ old('legende') }}"
+                    placeholder="type your legende" required>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Phone</label>
+                <input type="text" class="form-control" name="phone" value="{{ old('phone') }}"
+                    placeholder="type your phone" required>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Code Postale</label>
+                <input type="text" class="form-control" name="code_postale" value="{{ old('code_postale') }}"
+                    placeholder="type your Code Postale" required>
+            </div>
+
+            <div class="mb-3 mt-5">
+                <input type="submit" class="btn btn-success form-control" value="Submit"
+                     required>
+            </div>
+        </form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
