5caf3a63d94b4cc43d959178611b6ad2a6bf7f4b
Mohamed kaffouh <87676772+Medkaffouh@users.noreply.github.com>
2022-04-28 13:58:15 +0000

    save multi data into database

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -14,19 +14,31 @@ class CustomerController extends Controller
 
     public function store(Request $request){
         $data = $request->all();
-        dd($data);
+        // dd($data);
 
         $customer = new Customer;
         $customer->name = $data['name'];
         $customer->email = $data['email'];
         $customer->save();
 
-        $detailcustomer = new DetailCustomer;
-        $detailcustomer->user_id = $customer->id;
-        $detailcustomer->legende = $data['legende'];
-        $detailcustomer->phone = $data['phone'];
-        $detailcustomer->code_postale = $data['code_postale'];
-        $detailcustomer->save();
+        // $detailcustomer = new DetailCustomer;
+        // $detailcustomer->user_id = $customer->id;
+        // $detailcustomer->legende = $data['legende'];
+        // $detailcustomer->phone = $data['phone'];
+        // $detailcustomer->code_postale = $data['code_postale'];
+        // $detailcustomer->save();
+
+        if(count($data['legende']) > 0){
+            foreach($data['legende'] as $item => $value){
+                $data2 =array(
+                    'user_id' => $customer->id,
+                    'legende' => $data['legende'][$item],
+                    'phone' => $data['phone'][$item],
+                    'code_postale' => $data['code_postale'][$item],
+                );
+                DetailCustomer::create($data2);
+            }
+        }
 
         return redirect()->back()->with('status','Data Inserted successffuly');
     }
